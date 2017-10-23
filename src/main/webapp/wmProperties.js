76eac88a057215da1cffb3b30f2daa51f43b5756
apptest <appTest1@wavemaker.com>
2017-10-23 15:17:16 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_jgRfsFjyeu",
+  "homePage" : "Main",
+  "name" : "Auto_jgRfsFjyeu",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
